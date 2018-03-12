42ddb51b8434d8b0deeb1c82c883f34f8a25585e
sstolyarenko <svyatoslav.stolyarenko@webtech.in.ua>
2018-03-12 13:05:22 +0200

    Improve Pizza wizard

--- src/main/resources/js/PizzaOrderController.js
+++ src/main/resources/js/PizzaOrderController.js
@@ -5,28 +5,42 @@ angular
         controller.name = "name";
 
         //new pizza
-        controller.newPizza = {
-            size: 12,
-            ingredients: []
-        };
-        controller.newPizza = function (pizzaBase) {
-            return {
-                size: pizzaBase.size,
-                ingredients: [pizzaBase]
-            }
-        };
-
         controller.pizzaBases = [
             {size: 12, name: "12inc pizza base", price: 12},
-            {size: 15, name: "12inc pizza base", price: 15},
-            {size: 18, name: "12inc pizza base", price: 18}
+            {size: 15, name: "15inc pizza base", price: 15},
+            {size: 18, name: "18inc pizza base", price: 18}
         ];
-        //new pizza
 
-        controller.ingredients = [controller.newPizza(), controller.newPizza()];
-        controller.order = [controller.newPizza(), controller.newPizza()];
+        controller.add12IncPizza = controller.pizzaBases[0];
+        controller.add15IncPizza = controller.pizzaBases[1];
+        controller.add18IncPizza = controller.pizzaBases[2];
+        //new pizza end
 
-        controller.addPizza = function () {
-            controller.ingredients.concat(controller.newPizza());
+        controller.ingredients = [{name: "f", price: 11}, {name: "s", price: 11}, {name: "t", price: 2}];
+        controller.order = [];
+
+        controller.addIngredient = function (pizzaIndx, ingrIndx) {
+            var newIngr = {
+                name: controller.ingredients[ingrIndx].name,
+                price: controller.ingredients[ingrIndx].price
+            };
+            controller.order[pizzaIndx].ingredients.push(newIngr);
         };
+
+        controller.removeIngredient = function (pizzaIndx, ingrIndx) {
+            controller.order[pizzaIndx].ingredients.splice(ingrIndx, 1);
+        };
+
+        controller.addPizza = function (pizza) {
+            var newPizza = {
+                size: pizza.size,
+                name: pizza.name,
+                ingredients: pizza.ingredients || []
+            };
+            controller.order.push(newPizza);
+        };
+
+
+        //todo
+        controller.addPizza(controller.pizzaBases[1]);
     });
